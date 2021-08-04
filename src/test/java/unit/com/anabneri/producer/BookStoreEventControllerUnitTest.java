3fb082e8f2c8ef20b0b8da86a47dc90b1c245ed0
Ana Neri <ananeri32@hotmail.com>
2021-08-03 22:27:18 -0300

    adding unit tests
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

---
+++ src/test/java/unit/com/anabneri/producer/BookStoreEventControllerUnitTest.java
@@ -0,0 +1,89 @@
+package com.anabneri.producer;
+
+import com.anabneri.controller.BookStoreEventController;
+import com.anabneri.domain.Book;
+import com.anabneri.domain.BookStoreEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookStoreEventController.class)
+@AutoConfigureMockMvc
+public class BookStoreEventControllerUnitTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    BookStoreEventProducer bookStoreEventProducer;
+
+    @Test
+    void should_post_book_store_event() throws Exception {
+
+        // given
+        Book book = Book.builder()
+                .bookId(111)
+                .bookName("A culpa e das estrelas")
+                .bookAuthor("John Green")
+                .build();
+
+        BookStoreEvent bookStoreEvent = BookStoreEvent.builder()
+                .bookStoreEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(bookStoreEvent);
+
+        // when
+        when(bookStoreEventProducer.senBookStoreEvent_Approach2(isA(BookStoreEvent.class))).thenReturn(null);
+
+
+        // then
+        mockMvc.perform(post("/v1/bookstore-event")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+
+    @Test
+    void should_not_post_book_store_event_when_status_code_was_4xx() throws Exception {
+
+        // given
+        Book book = Book.builder()
+                .bookId(null)
+                .bookName("A culpa e das estrelas")
+                .bookAuthor(null)
+                .build();
+
+
+        BookStoreEvent bookStoreEvent = BookStoreEvent.builder()
+                .bookStoreEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(bookStoreEvent);
+
+        when(bookStoreEventProducer.senBookStoreEvent_Approach2(isA(BookStoreEvent.class))).thenReturn(null);
+
+        // expected
+        String expectedErrorMessage = "book.bookAuthor - must not be blank , book.bookId - must not be null";
+        mockMvc.perform(post("/v1/bookstore-event")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+                .andExpect(content().string(expectedErrorMessage));
+    }
+}
